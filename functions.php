e8f5ecb4ecc7dca884d48c79cb085ac1c5d91ad9
ubuntu <xiaoyaobilang@gmail.com>
2021-11-26 23:16:21 +0800

    change header photo size

--- functions.php
+++ functions.php
@@ -51,7 +51,7 @@ function dazzling_setup() {
    */
   add_theme_support( 'post-thumbnails' );
 
-  add_image_size( 'dazzling-featured', 730, 410, true );
+  add_image_size( 'dazzling-featured', 800, 600, true );
   add_image_size( 'tab-small', 60, 60 , true); // Small Thumbnail
 
   // This theme uses wp_nav_menu() in one location.
